702b6f251c24bc05320ae7232bfc524c6047d2a9
Rickard Nilsson <rickynils@gmail.com>
2020-09-18 17:27:08 +0200

    examples: Add simple flake example

---
+++ examples/flakes-simple/flake.nix
@@ -0,0 +1,15 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs/release-20.03";
+  };
+
+  outputs = { self, nixpkgs }: {
+
+    defaultPackage.x86_64-linux =
+      nixpkgs.legacyPackages.x86_64-linux.hello.overrideDerivation (drv: {
+        patches = (drv.patches or []) ++ [ ./hello.patch ];
+        doCheck = false;
+      });
+
+  };
+}
