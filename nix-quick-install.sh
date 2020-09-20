6e3b02cc4557bc3821cda8d868a79a54bf50b8ee
Rickard Nilsson <rickynils@gmail.com>
2020-09-20 23:54:15 +0200

    Use v2 in examples and bootstrapping

--- nix-quick-install.sh
+++ nix-quick-install.sh
@@ -22,11 +22,7 @@ if [ -n "$NIX_CONF" ]; then
 fi
 
 # Install nix in profile
-if [ -a /nix/bin ]; then
-  nix="$(dirname "$(realpath -m /nix/bin)")"
-else
-  nix="$(realpath -m /nix/.nix)"
-fi
+nix="$(realpath -m /nix/.nix)"
 MANPATH= . "$nix/etc/profile.d/nix.sh"
 "$nix/bin/nix-env" -i "$nix"
 
