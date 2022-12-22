a53f88ddb551dfcac8e12e9e7d31e0b4a49fc307
Rickard Nilsson <rickynils@gmail.com>
2022-12-22 21:39:20 +0100

    nix minor bumps: 2.11.0 -> 2.11.1, 2.3.15 -> 2.3.16

--- flake.nix
+++ flake.nix
@@ -4,15 +4,8 @@
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs-nix-2_12_0.url = "nixpkgs/593d839e8fadea1183e071186ae1b584792d4884";
-    nixpkgs-nix-2_11_0.url = "nixpkgs/6e56024e67fb31c07d3531965d6c9210e638e84b";
-    nixpkgs-nix-2_10_3.url = "nixpkgs/ed0e38f28d60055832397382c4ca471d2bc1b173";
-    nixpkgs-nix-2_9_2.url = "nixpkgs/8404dc74ef972c37009be6b20086ab7bd5e45f21";
-    nixpkgs-nix-2_8_1.url = "nixpkgs/eb78d30bf48c1064299d96082054bae587582f2e";
-    nixpkgs-nix-2_7_0.url = "nixpkgs/e3a73aed432da016f57230ac977cf515525e6cc5";
-    nixpkgs-nix-2_6_1.url = "nixpkgs/a4a3ef1016e6dddda8ebd3bde232ba5102a406e7";
     nixpkgs-nix-2_5_1.url = "nixpkgs/89f196fe781c53cb50fef61d3063fa5e8d61b6e5";
     nixpkgs-nix-2_4.url = "nixpkgs/e912008eef096f52f28cf87492830c54ef334eb4";
-    nixpkgs-nix-2_3_15.url = "nixpkgs/9bd0be76b2219e8984566340e26a0f85caeb89cd";
     nixpkgs-nix-2_2_2.url = "nixpkgs/5399f34ad9481849720d14605ce87b81abe202e9";
     nixpkgs-nix-2_2_2.flake = false;
     nixpkgs-nix-2_1_3.url = "nixpkgs/2c9265c95075170ad210ed5635ecffcd36db6b84";
@@ -25,15 +18,8 @@
     self,
     flake-utils,
     nixpkgs-nix-2_12_0,
-    nixpkgs-nix-2_11_0,
-    nixpkgs-nix-2_10_3,
-    nixpkgs-nix-2_9_2,
-    nixpkgs-nix-2_8_1,
-    nixpkgs-nix-2_7_0,
-    nixpkgs-nix-2_6_1,
     nixpkgs-nix-2_5_1,
     nixpkgs-nix-2_4,
-    nixpkgs-nix-2_3_15,
     nixpkgs-nix-2_2_2,
     nixpkgs-nix-2_1_3,
     nixpkgs-nix-2_0_4
@@ -78,15 +64,15 @@
         nix.version nix
       ) (
         [ nixpkgs-nix-2_12_0.legacyPackages.${system}.nix
-          nixpkgs-nix-2_11_0.legacyPackages.${system}.nix
-          nixpkgs-nix-2_10_3.legacyPackages.${system}.nix
-          nixpkgs-nix-2_9_2.legacyPackages.${system}.nix
-          nixpkgs-nix-2_8_1.legacyPackages.${system}.nix
-          nixpkgs-nix-2_7_0.legacyPackages.${system}.nix
-          nixpkgs-nix-2_6_1.legacyPackages.${system}.nix
+          nixpkgs-nix-2_12_0.legacyPackages.${system}.nixVersions.nix_2_11
+          nixpkgs-nix-2_12_0.legacyPackages.${system}.nixVersions.nix_2_10
+          nixpkgs-nix-2_12_0.legacyPackages.${system}.nixVersions.nix_2_9
+          nixpkgs-nix-2_12_0.legacyPackages.${system}.nixVersions.nix_2_8
+          nixpkgs-nix-2_12_0.legacyPackages.${system}.nixVersions.nix_2_7
+          nixpkgs-nix-2_12_0.legacyPackages.${system}.nixVersions.nix_2_6
           nixpkgs-nix-2_5_1.legacyPackages.${system}.nix
           nixpkgs-nix-2_4.legacyPackages.${system}.nix
-          nixpkgs-nix-2_3_15.legacyPackages.${system}.nix
+          nixpkgs-nix-2_12_0.legacyPackages.${system}.nixVersions.nix_2_3
           (import nixpkgs-nix-2_2_2 { inherit system; }).nix
           (import nixpkgs-nix-2_1_3 { inherit system; }).nix
         ] ++ lib.optionals (system == "x86_64-linux") [
