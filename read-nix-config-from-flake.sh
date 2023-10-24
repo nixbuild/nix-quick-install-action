e43c96cce6dec7145d0c7c250edcc8b110cb0311
Rickard Nilsson <rickynils@gmail.com>
2023-10-24 08:57:41 +0200

    load_nixConfig: Fix jq invocation error

--- read-nix-config-from-flake.sh
+++ read-nix-config-from-flake.sh
@@ -26,12 +26,12 @@ flake_url=${flake_url:="github:$OWNER_AND_REPO/$SHA"}
 # only fetch if not (locally) defined (e.g. for testing)
 if [ "$flake_file" = "$tmp" ]; then
   gh_err="$(mktemp)"
-  gh api 2>"$gh_err" >"$flake_file" \
+  gh_out="$(mktemp)"
+  gh api 2>"$gh_err" >"$gh_out" \
     -H "Accept: application/vnd.github+json" \
     -H "X-GitHub-Api-Version: 2022-11-28" \
     "/repos/$OWNER_AND_REPO/contents/flake.nix?ref=$SHA" || true
   gh_ec=$?
-
   if [ "$gh_ec" -ne 0 ]; then
     if grep -q "HTTP 404" "$gh_err"; then
       echo >&2 "No flake.nix found, not loading config"
@@ -41,7 +41,7 @@ if [ "$flake_file" = "$tmp" ]; then
       exit 1
     fi
   else
-    jq -r '.content|gsub("[\n\t]"; "")|@base64d' >"$flake_file" || exit 1
+    jq -r '.content|gsub("[\n\t]"; "")|@base64d' "$gh_out" >"$flake_file" || exit 1
   fi
 fi
 
