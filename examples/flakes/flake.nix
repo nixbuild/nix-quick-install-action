f17e4d844305757ec70af2cf0c0073d3538c893b
F. Emerson <t@utologi.cc>
2024-12-03 00:40:07 -0300

    docs: port flakes example to all supported archs

--- examples/flakes/flake.nix
+++ examples/flakes/flake.nix
@@ -1,18 +1,22 @@
 {
   inputs.nixpkgs.url = "nixpkgs/release-24.05";
 
-  outputs = { self, nixpkgs }: {
-
-    defaultPackage.x86_64-linux =
-      nixpkgs.legacyPackages.x86_64-linux.hello.overrideDerivation (drv: {
-        patches = (drv.patches or []) ++ [ ./hello.patch ];
-        doCheck = false;
-      });
-
+  outputs = { self, nixpkgs }:
+  let
+    forAllSystems = f:
+      nixpkgs.lib.genAttrs [
+        "aarch64-darwin"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ] (system: nixpkgs.legacyPackages.${system});
+  in {
+    defaultPackage = forAllSystems (pkgs: pkgs.hello.overrideDerivation (drv: {
+      patches = (drv.patches or [ ]) ++ [ ./hello.patch ];
+      doCheck = false;
+    }));
   };
 
   nixConfig = {
     allow-import-from-derivation = "true";
   };
- 
 }
